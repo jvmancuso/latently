7268239082f5fe19a66e9668912b5ce1521e3683
jvmancuso <jason@manc.us>
2018-12-19 14:43:31 -0500

    fix up script

--- recover.py
+++ recover.py
@@ -20,8 +20,9 @@ from PIL import Image
 
 
 def main(args):
+    truncation = args.truncation
     # Choose a random starting point
-    zp = tf.Variable(np.random.normal(size=(1,512)), dtype=tf.float32)
+    zp = tf.Variable(truncation * tf.truncated_normal([len(args.images), 128]))
     # Or if we know the original latent vector, we can start from it
     # start_zp = np.load(folder + "zp_start.npy")
     # zzp = np.empty((1,512))
@@ -30,6 +31,7 @@ def main(args):
 
     # Load the image for which we want to recover the latent vector
     # and crete an appropriate tensor for it
+    print("Loading images...")
     arrays = []
     for img_name in args.images:
         img = Image.open("{}/{}".format(args.source_dir, img_name))
@@ -38,10 +40,47 @@ def main(args):
         arrays.append(img_arr / 255.)
     img_np = np.concatenate(arrays, axis=0)
     fz = tf.Variable(img_np, tf.float32)
+    batch_size = len(args.images)
+    y_index = tf.random_uniform([batch_size], maxval=1000, dtype=tf.int32)
+    y = tf.Variable(tf.one_hot(y_index, 1000))
+
+    inputs = {k: tf.placeholder(v.dtype, v.get_shape().as_list(), k)
+          for k, v in module.get_input_info_dict().iteritems()}
+
+    input_z = inputs['z']
+    input_y = inputs['y']
+    input_trunc = inputs['truncation']
+
+    dim_z = input_z.shape.as_list()[1]
+    vocab_size = input_y.shape.as_list()[1]
+
+    def truncated_z_sample(batch_size, truncation=1., seed=None):
+        state = None if seed is None else np.random.RandomState(seed)
+        values = truncnorm.rvs(-2, 2, size=(batch_size, dim_z), random_state=state)
+        return truncation * values
+
+    def one_hot(index, vocab_size=vocab_size):
+        index = np.asarray(index)
+        if len(index.shape) == 0:
+            index = np.asarray([index])
+        assert len(index.shape) == 1
+        num = index.shape[0]
+        output = np.zeros((num, vocab_size), dtype=np.float32)
+        output[np.arange(num), index] = 1
+        return output
+
+    def one_hot_if_needed(label, vocab_size=vocab_size):
+        label = np.asarray(label)
+        if len(label.shape) <= 1:
+            label = one_hot(label, vocab_size)
+        assert len(label.shape) == 2
+        return label
 
     # Define the optimization problem
+    print("Loading generator...")
     generator = hub.Module("https://tfhub.dev/deepmind/biggan-512/2")
-    fzp = generator(zp)
+    print("Generating image from random latent vector...")
+    fzp = generator()
     loss = tf.losses.mean_squared_error(labels=fz, predictions=fzp)
 
     # Decayed gradient descent
@@ -52,23 +91,23 @@ def main(args):
                                                args.decay_rate)
     opt = tf.train.GradientDescentOptimizer(learning_rate)
     # Optimize on the variable zp
-    train = opt.minimize(loss, var_list=zp, global_step=global_step)
+    train = opt.minimize(loss, var_list=[zp, y], global_step=global_step)
 
     sess = tf.Session()
     sess.run(tf.global_variables_initializer())
     for i in range(args.iters): # Use more iterations (10000)
         # If we know the original latent vector, we can also compute
         # how far the recovered vector is from it
-        _, loss_value, zp_val, eta = sess.run((train, loss, zp, learning_rate))
+        _, loss_value, zp_val, eta = sess.run((train, loss, zp, y, learning_rate))
         print("%03d) eta=%03f, loss = %f" % (i, eta, loss_value))
     # Save the recovered latent vector
-    zp_val = sess.run(zp)
+    zp_val, y_val = sess.run(zp, y)
     for ix, img_name in enumerate(args.images):
         np.save("{}/{}.npy".format(args.dest_dir, img_name[:-4]), zp_val[ix])
 
     # Print out the corresponding image out of the recovered
     # latent vector
-    imgs = sess.run(generator(zp))
+    imgs = sess.run(generator({z: zp_val, y: y_val}))
     imgs = (imgs * 255).astype(np.uint8)
     Image.fromarray(imgs[0]).save("{}/{}.recv.jpg".format(args.dest_dir, image_name[:-4]))
 
@@ -80,6 +119,9 @@ if __name__ == "__main__":
                         help="directory containing source images")
     parser.add_argument('-D', '--dest-dir', type=str, default="latent_vectors",
                         help="directory to store recovered latent vectors")
+
+    parser.add_argument('-T', '--truncation', type=float, default=.5,
+                        help="BigGAN truncation factor")
 
     parser.add_argument('-i', '--iters', type=int, default=10000,
                         help="number of iterations of gradient descent")
